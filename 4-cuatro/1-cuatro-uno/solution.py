6d71c253c9c899316c1b200f29b1f5c8038733ec
kefir <knokh.ph@gmail.com>
2025-12-04 19:26:32 +0100

    init cuatro

---
+++ 4-cuatro/1-cuatro-uno/solution.py
@@ -0,0 +1,9 @@
+# Eine mögliche Lösung für den ersten Teil der vierten Aufgabe in Adnvent of Code 2025
+# https://adventofcode.com/2025/day/4
+
+def solution(file):
+  pass
+
+
+if "__main__" == __name__:
+  print(f"Solution: {solution("test-input.txt")}")
