91429761e250ac6ec59b8483d4e81fea1f088d87
kefir <knokh.ph@gmail.com>
2025-12-04 13:53:31 +0100

    solution for tres-uno

--- 3-tres/1-tres-uno/solution.py
+++ 3-tres/1-tres-uno/solution.py
@@ -1,3 +1,16 @@
 # Eine mögliche Lösung für den ersten Teil der driten Aufgabe in Adnvent of Code 2025
 # https://adventofcode.com/2025/day/3
 
+def solution(file):
+  joltage = 0
+  with open(file, 'r') as f:
+    for line in f:
+      line = line.replace("\n", "")
+      first_biggest = max(line[:-1])
+      first_biggest_index = line.index(first_biggest)
+      second_biggest = max(line[first_biggest_index+1:])
+      joltage += int(first_biggest + second_biggest)
+  return joltage
+
+if "__main__" == __name__:
+  print(f"Solution: {solution("real-input.txt")}")
