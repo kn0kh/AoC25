5b1fc4dea699f67fda311b4e36da753a0679d242
kefir <knokh.ph@gmail.com>
2025-12-04 23:26:33 +0100

    solution cuatro uno

--- 4-cuatro/1-cuatro-uno/solution.py
+++ 4-cuatro/1-cuatro-uno/solution.py
@@ -1,9 +1,34 @@
 # Eine mögliche Lösung für den ersten Teil der vierten Aufgabe in Adnvent of Code 2025
 # https://adventofcode.com/2025/day/4
 
+
 def solution(file):
-  pass
+  counter = 0
+  rolls_positions = set()
+
+  with open(file, "r") as f:
+    for y, line in enumerate(f):
+      row = line.replace("\n", "")
+
+      for x, r in enumerate(row):
+        if r == "@":
+          rolls_positions.add((x, y))
+
+    adjacent_counter = 0
+    for roll in rolls_positions:
+      for i in range(-1, 2):
+        for j in range(-1, 2):
+          if not (roll[0]+i < 0 or roll[0]+i == len(row) 
+                  or roll[1]+j < 0 or roll[1]+j > y 
+                  or (i, j) == (0, 0) or (roll[0]+i, roll[1]+j) not in rolls_positions):
+            adjacent_counter += 1
+
+      if adjacent_counter < 4:
+        counter += 1
+      adjacent_counter = 0
+
+  return counter
 
 
 if "__main__" == __name__:
-  print(f"Solution: {solution("test-input.txt")}")
+  print(f"Solution: {solution("real-input.txt")}")
