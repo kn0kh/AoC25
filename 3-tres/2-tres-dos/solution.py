9f76330560767b4498d789c42f8f8ac0472b81b7
kefir <knokh.ph@gmail.com>
2025-12-04 19:20:29 +0100

    solution tres dos

--- 3-tres/2-tres-dos/solution.py
+++ 3-tres/2-tres-dos/solution.py
@@ -2,9 +2,28 @@
 # https://adventofcode.com/2025/day/3
 
 def solution(file):
+  joltage = 0
+
   with open(file, 'r') as f:
     for line in f:
       bank = line.replace("\n", "")
+      bank_joltage = []
+
+      j = -1
+      for i in range(-11, 1):
+
+        if i == 0:
+          sample_biggest = max(bank[j+1:])
+          j += bank[j+1:].index(sample_biggest) + 1
+        else:
+          sample_biggest = max(bank[j+1:i])
+          j += bank[j+1:i].index(sample_biggest) + 1
+
+        bank_joltage.append(sample_biggest)
+      joltage += int(''.join(bank_joltage))
+       
+    return joltage
+  
 
 if "__main__" == __name__:
-  print(f"Solution: {solution("test-input.txt")}")
+  print(f"Solution: {solution("real-input.txt")}")
