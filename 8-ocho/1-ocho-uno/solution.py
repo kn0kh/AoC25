b6564183b798cbbad71930f00b72508979218a26
kefir <knokh.ph@gmail.com>
2025-12-08 19:55:32 +0100

    made a bit more readable

--- 8-ocho/1-ocho-uno/solution.py
+++ 8-ocho/1-ocho-uno/solution.py
@@ -2,7 +2,7 @@
 # https://adventofcode.com/2025/day/8
 
 def solution(file):
-  shortest = []
+  pairs = []
   with open(file, 'r') as f:
     coordinates = []
     for line in f:
@@ -11,48 +11,44 @@ def solution(file):
     for i, coordinate in enumerate(coordinates[:-1], start=1):
       for coord in coordinates[i:]:
         distance = ((((coordinate[0]-coord[0])**2)+((coordinate[1]-coord[1])**2)+((coordinate[2]-coord[2])**2))**0.5)
-        if len(shortest) < 1000:
-          shortest.append({"distance": distance, "from": coord, "to": coordinate})
-          shortest.sort(key=lambda item: item["distance"])
-        elif shortest[-1]["distance"] > distance:
-          shortest[-1] = ({"distance": distance, "from": coord, "to": coordinate})
-          shortest.sort(key=lambda item: item["distance"])
+        if len(pairs) < 1000:
+          pairs.append({"distance": distance, "from": coord, "to": coordinate})
+          pairs.sort(key=lambda item: item["distance"])
+        elif pairs[-1]["distance"] > distance:
+          pairs[-1] = ({"distance": distance, "from": coord, "to": coordinate})
+          pairs.sort(key=lambda item: item["distance"])
 
-    circuit = [[shortest[0]["from"], shortest[0]["to"]]]
-
-    for s in shortest[1:]:
-      where = False
+    circuit = []
+    for pair in pairs:
+      there = False
       for i, c in enumerate(circuit):
-        if not(s["from"] in c and s["to"] in c):
-          if s["from"] in c:
-            if not where:
-              circuit.append([s["to"]]+c)
-            else:
-              circuit.pop(circuit.index(where))
-              c.remove(s["from"])
-              where += c
-              circuit.append(where)
-            circuit.pop(i)
-            where = circuit[-1]
-          elif s["to"] in c:
-            if not where:
-              circuit.append([s["from"]]+c)
-            else:
-              circuit.pop(circuit.index(where))
-              c.remove(s["to"])
-              where += c
-              circuit.append(where)
-            circuit.pop(i)
-            where = circuit[-1]
+        if pair["from"] in c and pair["to"] in c:
+          there = c
         else:
-          where = c
-      if not where:
-        circuit.append([s["from"], s["to"]])
+          if pair["from"] in c:
+            if not there:
+              circuit.append([pair["to"]]+c)
+            else:
+              circuit.pop(circuit.index(there))
+              c.remove(pair["from"])
+              circuit.append(there + c)
+            circuit.pop(i)
+            there = circuit[-1]
+          elif pair["to"] in c:
+            if not there:
+              circuit.append([pair["from"]]+c)
+            else:
+              circuit.pop(circuit.index(there))
+              c.remove(pair["to"])
+              circuit.append(there + c)
+            circuit.pop(i)
+            there = circuit[-1]
+      if not there:
+        circuit.append([pair["from"], pair["to"]])
 
     circuit.sort(key=len, reverse=True)
     solution = 1
-    for x in circuit[:3]:
-      solution *= len(x)
+    for x in circuit[:3]: solution *= len(x)
 
     return solution
     
