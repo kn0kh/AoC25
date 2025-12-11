f875521e76eb5e2620762d513d401269b368fa3d
kefir <knokh.ph@gmail.com>
2025-12-11 19:49:23 +0100

    finally a solution

--- 9-nueve/2-nueve-dos/solution.py
+++ 9-nueve/2-nueve-dos/solution.py
@@ -2,67 +2,106 @@
 # https://adventofcode.com/2025/day/9
 
 def solution(file):
+  x_red, y_red = [], []
   red_tiles = []
-  green_borders = []
   largest_area = 0
   with open(file, 'r') as f:
-    for coordinate in f:
-      red_tiles.append(tuple(map(int, coordinate.split(","))))
+    for coordinates in f:
+      coordinate = (tuple(map(int, coordinates.split(","))))
+      red_tiles.append(coordinate)
+      x_red.append(coordinate[0])
+      y_red.append(coordinate[1])
 
-    for i, red_tile in enumerate(red_tiles, start=1):
-      i %= len(red_tiles)
-      if red_tile[0] == red_tiles[i][0]:
-        borders = [red_tile[1], red_tiles[i][1]]
-        borders.sort()
-        for y in range(borders[0], borders[1]+1):
-          green_borders.append((red_tile[0], y))
+  raw_red_tiles = red_tiles.copy()
+
+  x_sorted = list(sorted(set(x_red)))
+  y_sorted = list(sorted(set(y_red)))
+  x_map = dict()
+  y_map = dict()
+
+  new_coord = 0
+  for xs in x_sorted:
+    x_map[str(xs)] = new_coord
+    new_coord += 2
+
+  new_coord = 0
+  for ys in y_sorted:
+    y_map[str(ys)] = new_coord
+    new_coord += 2
+
+  for i, rt in enumerate(red_tiles):
+    red_tiles[i] = (x_map[str(rt[0])], y_map[str(rt[1])])
+
+  green_tiles = []
+  for i, red_tile in enumerate(red_tiles, start=1):
+    print(f"Getting green tiles {i}/{len(green_tiles)}")
+    i %= len(red_tiles)
+    if red_tile[0] == red_tiles[i][0]:
+      ranges = sorted([red_tile[1], red_tiles[i][1]])
+      for y in range(ranges[0]+1, ranges[1]):
+        green_tiles.append((red_tile[0], y))
+    else:
+      ranges = sorted([red_tile[0], red_tiles[i][0]])
+      for x in range(ranges[0]+1, ranges[1]):
+        green_tiles.append((x, red_tile[1]))
+
+  bin_map = []
+  for y in range(len(y_map)*2):
+    print(f"Making bit map {y}/{range(len(y_map)*2)[-1]}")
+    bin_map.append([])
+    for x in range(len(x_map)*2):
+      if (x,y) in red_tiles or (x,y) in green_tiles:
+        bin_map[y].append(bin(1))
       else:
-        borders = [red_tile[0], red_tiles[i][0]]
-        borders.sort()
-        for x in range(borders[0], borders[1]+1):
-          green_borders.append((x, red_tile[1]))
+        bin_map[y].append(bin(0))
 
-    green_borders.sort(key=lambda x: x[0])
-
-    green_ranges = dict()
-    for green_tile in green_borders:
-          borders = [min([green_tile[1],green_ranges.get(str(green_tile[0]), [green_tile[1]])[0]]),
-                      max([green_tile[1], green_ranges.get(str(green_tile[0]), [green_tile[1]])[-1]])]
-          green_ranges[str(green_tile[0])] = range(borders[0], borders[1]+1)
-
-    for i, r_tile in enumerate(red_tiles, start=1):
-      print(f"{i}/{len(red_tiles)}")
-      for rt in red_tiles[i:]:
-        area = abs(r_tile[0]-rt[0]+1) * abs(r_tile[1]-rt[1]+1)
-        if area > largest_area:
-          inside = True
-          x_borders = [r_tile[0], rt[0]]
-          y_borders = [r_tile[1], rt[1]]
-          x_borders.sort()
-          y_borders.sort()
-          for x in range(x_borders[0]+1, x_borders[1]):
-            if r_tile[1] not in green_ranges.get(str(x), []):
-              inside = False
-              break
-            if rt[1] not in green_ranges.get(str(x), []):
-              inside = False
-              break
-          if inside:
-            for y in range(y_borders[0]+1, y_borders[1]):
-              if y not in green_ranges.get(str(r_tile[0]), []):
-                inside = False
-                break
-              if y not in green_ranges.get(str(rt[0]), []):
-                inside = False
-                break
-          if inside:
-            largest_area = area
-
-
-
-  return largest_area 
+  last_edge = max(raw_red_tiles, key=lambda item: item[0] + item[1]) 
+  print("flooding the shape") 
+  floodFill(bin_map, y_map[str(last_edge[1])]-1, x_map[str(last_edge[0])]-1)
 
   
+  for i, r_tile in enumerate(raw_red_tiles, start=1):
+    for rt in raw_red_tiles[i:]:
+      area = (abs(r_tile[0]-rt[0])+1)*(abs(r_tile[1]-rt[1])+1)
+      if area > largest_area:
+        x_border = sorted([x_map[str(r_tile[0])], x_map[str(rt[0])]])
+        y_border = sorted([y_map[str(r_tile[1])], y_map[str(rt[1])]])
+        inside = True
+        for y in range(y_border[0], y_border[1]+1):
+          for x in range(x_border[0], x_border[1]+1):
+            if bin_map[y][x] != bin(1):
+              inside = False
+              break
+          if not inside:
+            break
+        if inside:
+          largest_area = area
+  
+  return largest_area
+
+
+def floodFill(image, sr, sc):
+  rows, cols = len(image), len(image[0])
+  start_pixel = image[sr][sc]
+  
+  if start_pixel == bin(1):
+    return
+  
+  stack = [(sr, sc)]
+  image[sr][sc] = bin(1)
+  count = 0
+
+  while stack:
+    count += 1
+    print(f"Filled in {count} tiles")
+    r, c = stack.pop()
+    for dr, dc in [1, 0], [0, 1], [-1, 0], [0, -1]:
+      nr, nc = dr + r, dc + c
+      if  0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == start_pixel:
+        image[nr][nc] = bin(1)
+        stack.append((nr, nc))
+
+  return image
 
 
 if "__main__" == __name__:
